612bc8251934eb195b9934e4d658ec2700f30567
github username <github email address>
2017-03-06 16:40:10 -0500

    Done.

--- spec/views/partials_spec.rb
+++ spec/views/partials_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe "the navbar partial" do
   it "renders the navbar correctly" do
     render "layouts/navbar"
-   
+
     expect(response).to include('Programmers We L<span class="glyphicon glyphicon-heart"></span>ve')
   end
 end
@@ -36,19 +36,6 @@ describe "the programmers index page" do
   it "uses shorthand syntax for rendering the collection of programmers" do
     expect(programmers_index).to include("<%= render @programmers %>")
   end
-end
 
-# BONUS: Create a partial that renders a single attribute of a programmer
-xdescribe "the attribute partial" do
-  let(:programmer) { FactoryGirl.create(:programmer) }
-
-  it "renders any attribute of the programmer" do
-      view.lookup_context.prefixes = %w[programmers]
-      assign(:programmer, programmer)  
-      render :template => "programmers/show.html.erb"
-      expect(response).not_to include("programmer.send(attribute)")
-      expect(response).not_to include("programmer.home_country")
-    
-  end
 
 end
